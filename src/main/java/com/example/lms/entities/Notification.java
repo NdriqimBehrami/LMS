583714f9b9487823db1c3968b438edf147371cd0
Dell-Center <ft46227@ubt-uni.net>
2022-04-29 23:57:27 +0200

    testnotificationclass

--- src/main/java/com/example/lms/entities/Notification.java
+++ src/main/java/com/example/lms/entities/Notification.java
@@ -1,5 +1,41 @@
 package com.example.lms.entities;
 
-public class Notification {
 
+import java.time.LocalDate;
+
+public class Notification {
+    private LocalDate notificationDate;
+    private LocalDate validUntilDate;
+    private String notificationMessage;
+
+
+    public Notification(LocalDate notificationDate, LocalDate validUntilDate, String notificationMessage) {
+        this.notificationDate = notificationDate;
+        this.validUntilDate = validUntilDate;
+        this.notificationMessage = notificationMessage;
+    }
+
+    public LocalDate getNotificationDate() {
+        return notificationDate;
+    }
+
+    public void setNotificationDate(LocalDate notificationDate) {
+        this.notificationDate = notificationDate;
+    }
+
+    public String getNotificationMessage() {
+        return notificationMessage;
+    }
+
+    public void setNotificationMessage(String notificationMessage) {
+        this.notificationMessage = notificationMessage;
+    }
+
+    public void setValidUntilDate(LocalDate validUntilDate) {
+        this.validUntilDate = validUntilDate;
+    }
+
+    public LocalDate getValidUntilDate() {
+        return validUntilDate;
+    }
 }
