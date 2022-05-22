41b61e30f0c49801d654ab03a5ae9a97e3c64724
Ndriqim <ndriqimib@gmail.com>
2022-05-22 23:21:36 +0200

    init

---
+++ src/main/java/com/example/lms/services/UserService.java
@@ -0,0 +1,74 @@
+package com.example.lms.services;
+
+import com.example.lms.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository usRepo;
+
+    public void save(User user) {
+        usRepo.save(user);
+    }
+
+    public void saveById(Long userId) {
+        User user = usRepo.findById(userId).get();
+        usRepo.save(user);
+    }
+
+    public User findById(long id) {
+        User user = usRepo.findById(id).get();
+        return user;
+    }
+
+    public List<User> findAll(){
+        return (List<User>) usRepo.findAll();
+    }
+
+    public List<User> userSearcher(String firstName, String lastName){
+        if (firstName != null && lastName != null) return getByFullName(firstName, lastName);
+        else if (firstName == null && lastName != null) return getByLastName(lastName);
+        else if (firstName != null && lastName == null) return getByFirstName(firstName);
+        else return new ArrayList<User>();
+    }
+
+    public List<User> getByFirstName(String firstName){
+        List<User> users = new ArrayList<User>();
+        for (User user : usRepo.findAll()) {
+            if (user.getFirstName().toLowerCase().contains(firstName.toLowerCase())) {
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+    public List<User> getByLastName(String lastName){
+        List<User> users = new ArrayList<User>();
+        for (User user : usRepo.findAll()) {
+            if(user.getLastName().toLowerCase().contains(lastName.toLowerCase())) {
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+    public List<User> getByFullName(String firstName, String lastName){
+        List<User> users = new ArrayList<User>();
+        for (User user : usRepo.findAll()) {
+            if (user.getFirstName().toLowerCase().contains(firstName.toLowerCase()) &&
+                    user.getLastName().toLowerCase().contains(lastName.toLowerCase())) {
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+}
+
